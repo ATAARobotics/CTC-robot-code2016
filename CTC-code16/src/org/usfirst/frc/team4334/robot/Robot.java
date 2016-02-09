484e6d9cfa01dd2c3451286d7c4f13fd725431db
aylabrooke <abccalgarygirl@gmail.com>
2016-02-09 13:43:53 -0700

    Added basic drivetrain code and constants class. Added .gitignore.
    Add .gitignore
    
    Remove binaries
    
    Remove jar files

--- CTC-code16/src/org/usfirst/frc/team4334/robot/Robot.java
+++ CTC-code16/src/org/usfirst/frc/team4334/robot/Robot.java
@@ -1,10 +1,19 @@
 package org.usfirst.frc.team4334.robot;
 
+import acilibj.actuators.SuperJoystickModule;
+import acilibj.actuators.TankDrivetrain;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
 
 public class Robot extends SampleRobot 
 {
-
+	Victor R1 = new Victor(Constants.RIGHT_MOTOR);
+	Victor L1 = new Victor(Constants.LEFT_MOTOR);
+	SuperJoystickModule driver = new SuperJoystickModule(Constants.JOYSTICK_1);
+	
+	TankDrivetrain drivetrain;
+	
     public Robot() 
     {
        
@@ -12,17 +21,23 @@ public class Robot extends SampleRobot
     
     public void robotInit() 
     {
-     
+    	drivetrain = new TankDrivetrain(L1, R1);
     }
     
     public void autonomous()
     {
-   
+    	
     }
     
     public void operatorControl()
     {
-      
+    	while(isOperatorControl() && isEnabled())
+    	{
+    		drivetrain.getHalo(driver.getAxisWithDeadzone(4, 0.15, false),
+    						driver.getAxisWithDeadzone(1, 0.15, true), 1, 0.8);
+    		
+    		Timer.delay(0.05);
+    	}
     }
     
     public void test() 
